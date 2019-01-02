10fb79e6b44c72d3672387e3a8e4ad508aac7744
DESKTOP-QT8F8OK\hernan <hernag_09@hotmail.com>
2019-01-02 18:48:31 -0300

    three commit

--- src/handler/handler.js
+++ src/handler/handler.js
@@ -1,7 +1,7 @@
 function mostrarTabla(req, res) {
     req.getConnection((err, conn) => {
         if (err) res.status(500).send(`${err}`)
-        conn.query('select * from news', (err, result) => {
+        conn.query('select * from news order by "Nombre"', (err, result) => {
             res.render('index', { result })
 
         })
@@ -22,20 +22,44 @@ function enviarDatos(req, res) {
 
 function borrarId(req, res) {
     let id = req.params.id
-    console.log(id)
+
     req.getConnection((err, conn) => {
         conn.query('DELETE FROM news WHERE id = ?', [id], (err, result) => {
-
+            console.log(result)
             res.redirect('/news')
         })
     })
 }
 
+function edit(req, res) {
+    let id = req.params.id
 
+    req.getConnection((err, conn) => {
+        conn.query('select * from news where id = ?', [id], (err, result) => {
+            res.render('update', { data: result[0] })
+        })
+
+    })
+}
+
+function update(req, res) {
+    let id = req.params.id
+    let cuerpo2 = req.body
+    req.getConnection((err, conn) => {
+        conn.query('update news set ? where id = ?', [cuerpo2, id], (err, result) => {
+
+            res.redirect('/news')
+
+        })
+
+    })
+}
 
 
 module.exports = {
     mostrarTabla,
     enviarDatos,
-    borrarId
+    borrarId,
+    edit,
+    update
 }
--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,8 @@ const handler = require('../handler/handler')
 
 api.get('/news', handler.mostrarTabla)
 api.post('/ad', handler.enviarDatos)
-    // esta ruta del delete , le habia puesto con el vervo delete pero tampoco me funca el video que mire lo hizo tipo asi con get no se por que.
+api.get('/edit/:id', handler.edit)
+api.post('/update/:id', handler.update)
 api.get('/delete/:id', handler.borrarId)
 
 
